42aae165e64d4c6e35d8ab3d6170e9dfca2cfeb7
liuchen <liuchen@iiydeMacBook-Pro.local>
2015-01-26 19:54:51 +0800

    this is a python code for phone number attribution

---
+++ phonenumber.py
@@ -0,0 +1,45 @@
+import mysql.connector 
+import urllib2
+import json
+import time
+
+config = {
+	'user': 'demo',
+	'password': 'demo',
+	'host': 'localhost',
+	'database': 'rui_site',
+}
+
+try:
+	db = mysql.connector.connect(**config)
+except Exception , e:
+	print e;
+
+
+for i in range(0,10):
+	for j in range(0,10):
+		for k in range(0,10):
+			for m in range(0,10):
+				innerfix = ''+`i`+`j`+`k`+`m`
+				prefix = "http://opendata.baidu.com/api.php?query=138"
+				suffix = "2232&co=&resource_id=6004&t=1422254015296&ie=utf8&oe=gbk&cb=op_aladdin_callback&format=json&tn=baidu&cb=jQuery110208778024467173964_1422254010331&_=1422254010334"
+	 			url = prefix + innerfix + suffix;
+	 			try:
+	 				print "start..." + innerfix
+					response = urllib2.urlopen(url)
+					html = response.read()
+					lpos = html.find("[")
+					rpos = html.rfind("]")
+					html1 =  html[lpos:rpos+1]
+					data = json.loads(html1,'gb2312')
+					result =  data[0]["city"]
+					cursor = db.cursor()
+					cursor.execute("insert into attributions(number, place) values ('"+innerfix +"','"+result+"')")
+					db.commit()
+					print "end..." + innerfix
+					time.sleep(0.01)
+				except Exception , e:
+					print e
+
+
+db.close()
